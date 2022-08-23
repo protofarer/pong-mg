136dd6094e01595e1c46bd7591de5d5776f845b0
Kenny Baron <kennybaron@fastmail.com>
2022-08-23 18:19:58 -0400

    new constants class

---
+++ Constants.cs
@@ -0,0 +1,6 @@
+public class Constants {
+    public const int VIRTUAL_WIDTH = 400;
+    public const int VIRTUAL_HEIGHT = 250;
+    public const int WINDOW_WIDTH = VIRTUAL_WIDTH * 4;
+    public const int WINDOW_HEIGHT = VIRTUAL_HEIGHT * 4;
+}
